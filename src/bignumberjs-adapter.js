afcc552a13cdd0441952820875757b6dd6720e62
Javier Cejudo <javier@javiercejudo.com>
2015-07-09 01:52:39 +1000

    feat(compat): make compatible with LAP 3

--- src/bignumberjs-adapter.js
+++ src/bignumberjs-adapter.js
@@ -2,6 +2,7 @@
 
 'use strict';
 
+var identity = require('lodash.identity');
 var bignumber = require('bignumber.js');
 
 module.exports = {
@@ -14,7 +15,7 @@ module.exports = {
   div: div,
   toString: toString,
   valueOf: toString,
-  toJSON: toString
+  parseInput: identity
 };
 
 function getPrecision(Bignumber) {
